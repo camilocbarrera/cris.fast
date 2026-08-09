f446a2e48f9a67806735c1acde054357095e338e
Cris <cristian.correa.cs@gmail.com>
2026-08-08 23:38:34 -0500

    Committing changes for app/terminal/opengraph-image.tsx

--- app/terminal/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import { OG_ALT, OG_SIZE, renderTerminalOg } from "./og-card"
-
-export const size = OG_SIZE
-export const contentType = "image/png"
-export const alt = OG_ALT
-
-export default function Image() {
-  return renderTerminalOg()
-}
